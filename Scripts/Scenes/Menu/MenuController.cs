d739f23c43e1bd9359b382429d02035eb9dc8515
Renata Falguera Gonçalves <renata.falguera@gmail.com>
2018-11-08 14:47:04 -0200

    Update MenuController.cs

--- Scripts/Scenes/Menu/MenuController.cs
+++ Scripts/Scenes/Menu/MenuController.cs
@@ -6,6 +6,7 @@ public class MenuController : MonoBehaviour {
 
     private AudioSource _buttonsound;
 
+	//If the "start" button is pressed it redirects to the instruction screen
 	public void Start_Game()
     {
         _buttonsound = GameObject.Find("Start_Button").GetComponent<AudioSource>();
